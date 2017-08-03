c9beb21649976b973bb2457e060aa5895240967e
mashelw <Mashelwaiz@gmail.com>
2017-08-02 21:08:38 -0700

    added friends array data

---
+++ data/friends.js
@@ -0,0 +1,86 @@
+var friends = {
+  name:"Ross Geller",
+  photo:"https://vignette3.wikia.nocookie.net/friends/images/8/89/Square_Ross.jpg/revision/latest?cb=20111216200027":[
+      1,
+      3,
+      5,
+      1,
+      2,
+      2,
+      5,
+      1,
+      5,
+      1
+      
+    ],
+    name:"Rachel",
+    photo:"https://vignette3.wikia.nocookie.net/friends/images/8/89/Square_Ross.jpg/revision/latest?cb=20111216200027":[
+      5,
+      2,
+      5,
+      2,
+      2,
+      1,
+      2,
+      5,
+      4,
+      1
+      
+    ],
+    name:"Monica",
+    photo:"https://vignette3.wikia.nocookie.net/friends/images/8/89/Square_Ross.jpg/revision/latest?cb=20111216200027":[
+      3,
+      5,
+      5,
+      1,
+      2,
+      2,
+      1,
+      2,
+      4,
+      2
+      
+    ],
+    name:"Joey",
+    photo:"https://vignette3.wikia.nocookie.net/friends/images/8/89/Square_Ross.jpg/revision/latest?cb=20111216200027":[
+      2,
+      1,
+      5,
+      5,
+      1,
+      3,
+      1,
+      5,
+      1,
+      2
+      
+    ],
+    name:"Chandler",
+    photo:"https://vignette3.wikia.nocookie.net/friends/images/8/89/Square_Ross.jpg/revision/latest?cb=20111216200027":[
+      1,
+      2,
+      5,
+      1,
+      2,
+      5,
+      1,
+      2,
+      5,
+      2
+      
+    ]
+    name:"Pheobe",
+    photo:"https://vignette3.wikia.nocookie.net/friends/images/8/89/Square_Ross.jpg/revision/latest?cb=20111216200027":[
+      2,
+      2,
+      5,
+      2,
+      5,
+      3,
+      2,
+      1,
+      1,
+      5
+      
+    ]
+}
